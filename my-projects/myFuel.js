795bc84af6d90548b29c17c43f8c5f2553df96dc
#Naruto61029160203 <jose_d_avila@homedepot.com>
2020-06-17 15:23:33 -0700

    Refactored myFuel and added test

--- my-projects/myFuel.js
+++ my-projects/myFuel.js
@@ -2,15 +2,13 @@ let totalFuel =0; // this sets totalFuel to zero
 let arrayFuel = [12, 14, 1969, 100756]; // Needs fixing 
 let arrayLength = arrayFuel.length; 
 
-//function myFuel{
-//return
-
-for (let i = 0; i < arrayLength; i++) {
-myFuel = (Math.floor(arrayFuel/3)-2 +myFuel);
-//totalFuel += myFuel;
+const myFuel = (mass) => {
+//     for (let i = 0; i < arrayLength; i++) {
+        return Math.floor(mass/3)-2;    
+// }
 }
-
-//}
 
 totalFuel = myFuel;
 console.log(totalFuel); 
+
+module.exports = myFuel
--- my-projects/test.js
+++ my-projects/test.js
@@ -1,1 +1,19 @@
-const assert = require('');
+const myFuel = require("./myFuel.js");
+describe("myFuel()", () => {
+    test("Should calculate myFuel mass 12 ", () => {
+        expect(myFuel(12)).toBe(2)
+    
+    });
+    test("Should calculate myFuel mass 14 ", () => {
+        expect(myFuel(14)).toBe(2)
+    
+    });
+    test("Should calculate myFuel mass 1969 ", () => {
+        expect(myFuel(1969)).toBe(654)
+
+    });
+    test("Should calculate myFuel mass 100756 ", () => {
+        expect(myFuel(100756)).toBe(33583)
+
+    });
+})
