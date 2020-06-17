23412a2cf10caafbef18c23158de40e9c46efd8d
#Naruto61029160203 <jose_d_avila@homedepot.com>
2020-06-17 11:12:51 -0700

    first commit

---
+++ my-projects/myFuel.js
@@ -0,0 +1,16 @@
+let totalFuel =0; // this sets totalFuel to zero
+let arrayFuel = [12, 14, 1969, 100756]; // Needs fixing 
+let arrayLength = arrayFuel.length; 
+
+//function myFuel{
+//return
+
+for (let i = 0; i < arrayLength; i++) {
+myFuel = (Math.floor(arrayFuel/3)-2 +myFuel);
+//totalFuel += myFuel;
+}
+
+//}
+
+totalFuel = myFuel;
+console.log(totalFuel); 
